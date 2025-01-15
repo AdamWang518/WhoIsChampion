ec99c2730ff34fb33424068c795db0dea4e1213b
AdamWang518 <dodoga518@gmail.com>
2025-01-15 23:45:26 +0800

    start double

--- WhoIsChampion/Arrange.cs
+++ WhoIsChampion/Arrange.cs
@@ -10,9 +10,10 @@ namespace WhoIsChampion
 {
     internal class Arrange
     {
-        public void Basic(List<Player> playerList) {
+        public void Basic(List<Player> playerList)
+        {
             Fight fight = new Fight();
-            
+
             while (playerList.Count > 1)
             {
                 List<Player> nextRoundPlayer = new List<Player>();
@@ -27,7 +28,8 @@ namespace WhoIsChampion
             }
             Console.WriteLine($"比賽結束，{playerList[0].Name}獲得最終勝利");
         }
-        public List<Player> Single(List<Player> playerList) {
+        public List<Player> Single(List<Player> playerList)
+        {
             List<Player> result = new List<Player>();
             List<Player> seedList;
             Fight fight = new Fight();
@@ -42,19 +44,38 @@ namespace WhoIsChampion
             result.AddRange(seedList);
             return result;
         }
-        public (List<Player>, List<Player>) getSeed(List<Player> playerList) {
+        public List<Player> Double(List<Player> playerList)
+        {
+            List<Player> winnersBracket = new List<Player>(playerList); // 勝者組
+            List<Player> losersBracket = new List<Player>();            // 敗者組
+            List<Player> seedList;
+            List<Player> result = new List<Player>();
+            Fight fight = new Fight();
+            (playerList, seedList) = getSeed(playerList);
+            for (int i = 0; i < playerList.Count; i += 2)
+            {
+                Player winner = fight.DeathMatch(playerList[i], playerList[i + 1]);
+                Console.WriteLine($"{playerList[i].Name}對戰{playerList[i + 1].Name}");
+                Console.WriteLine($"{winner.Name}獲得勝利");
+                result.Add(winner);
+            }
+            result.AddRange(seedList);
+            return result;
+        }
+        public (List<Player>, List<Player>) getSeed(List<Player> playerList)
+        {
             List<Player> seedList = new List<Player>();
             int seedLength = FindClosestPowerOfTwo(playerList.Count());
-            
+
             (playerList, seedList) = SplitList(playerList, seedLength);
             foreach (Player player in seedList)
             {
                 Console.WriteLine($"{player.Name}獲選為種子");
             }
-           
-            return (playerList,seedList);
+
+            return (playerList, seedList);
         }
-       (List<Player>, List<Player>) SplitList(List<Player> list, int index)
+        (List<Player>, List<Player>) SplitList(List<Player> list, int index)
         {
             List<Player> firstPart = list.Take(index).ToList();
             List<Player> secondPart = list.Skip(index).ToList();
--- WhoIsChampion/FightGame.cs
+++ WhoIsChampion/FightGame.cs
@@ -13,12 +13,52 @@ namespace WhoIsChampion
         // 然後StartGame 去拿到Player List, 就可以在 StartGame 裡面進行遊玩
         // 並且讓使用者決定要使用循環賽還是單淘汰賽 (請使用enum來做)
         // 最後做完之後透過ShowWinner 顯示最終勝利玩家
-        public List<Player> playerList=new List<Player>();
-        public void createPlayer() {
+        public List<Player> playerList = new List<Player>();
+        public void createPlayer()
+        {
             int numberOfPlayer = 0;
-            numberOfPlayer=int.Parse(Console.ReadLine());
-            for (int i = 0; i < numberOfPlayer; i++) {
-                playerList.Add(new Player(i,Console.ReadLine()));
+            Console.WriteLine("請輸入玩家數量");
+            numberOfPlayer = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfPlayer; i++)
+            {
+                playerList.Add(new Player(i, Console.ReadLine()));
+            }
+        }
+        public void setGameType()
+        {
+            Console.WriteLine("請輸入遊戲類型");
+            Console.WriteLine("1.單淘汰賽");
+            Console.WriteLine("2.雙淘汰賽");
+            Console.WriteLine("3.循環賽");
+            FightGameType gameType;
+            while (true)
+            {
+                Console.Write("輸入對應的數字: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int gameTypeNum) && Enum.IsDefined(typeof(FightGameType), gameTypeNum))
+                {
+                    gameType = (FightGameType)gameTypeNum;
+                    Console.WriteLine($"選擇的遊戲類型是: {gameType}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("無效輸入，請輸入 1, 2 或 3。");
+                }
+            }
+
+            switch (gameType)
+            {
+                case FightGameType.單淘汰賽:
+                    Console.WriteLine("已設定為單淘汰賽。");
+                    break;
+                case FightGameType.雙淘汰賽:
+                    Console.WriteLine("已設定為雙淘汰賽。");
+                    break;
+                case FightGameType.循環賽:
+                    Console.WriteLine("已設定為循環賽。");
+                    break;
             }
         }
     }
